14fd2fc66aa190f042c14baa98bf9de67ab8b682
thinkingmonks <rishal@thinkingmonks.com>
2025-01-26 22:02:21 +0530

    Code Updated

--- assets/script/script.js
+++ assets/script/script.js
@@ -182,6 +182,47 @@ closeBtn.addEventListener("click", closePopup);
     });
 });
 
+
+// Define content for each home-box
+const contentData = {
+  "home-image1": {
+    head: "Indian Music Experience Museum",
+    des: "The Indian Music Experience's grand launch was a landmark celebration, merging tradition with innovation. Highlighted by an extraordinary performance from Ustad Zakir Hussain's ensemble, in collaboration with Louiz Banks. This momentous occasion brought music enthusiasts together to experience the confluence of heritage and contemporary artistry."
+  },
+  "home-image2": {
+    head: "Ravi Shankar @100",
+    des: "We took pride in honoring the centenary of the legendary Pandit Ravi Shankar with an immersive tribute. The event celebrated his unparalleled legacy through a thoughtfully curated exhibition, presenting iconic milestones from his journey. This celebration offered attendees an experiential dive into the life and music of one of India's most celebrated maestros."
+  },
+  "home-image3": {
+    head: "Brigade Showcase",
+    des: "For over seven years, we has played a pivotal role in conceptualizing and executing the Brigade Showcase in Bangalore. With a focus on creating engaging, experiential ideas that align with the brand's promise, the showcase has evolved into a standout platform for customers to experience the Brigade brand’s vision and offerings in an innovative way."
+  },
+  "home-image4": {
+    head: "Bangalore Design Week",
+    des: "We played a key role in shaping Bangalore’s first-ever Design Week, a platform celebrating creativity and innovation. BDW serves as a vibrant hub for Indian and global designers to connect, collaborate, and exchange ideas. The event underscores Bangalore’s identity as a design-forward city, promoting dialogue within the design community."
+  },
+  "home-image5": {
+    head: "Career Utsav",
+    des: "Career Utsav, Bangalore's leading education fair, has become a beacon of guidance for students exploring diverse career paths. With workshops and seminars by industry experts, the event showcases both conventional and cutting-edge professions. Since its inception in 2013, it has inspired over half a million students from India and abroad, helping them navigate critical career decisions."
+  },
+  // Add more entries for other home-boxes (hb3, hb4, hb5)
+};
+
+// Add click event listeners to all home-boxes
+document.querySelectorAll('.home-box').forEach(homeBox => {
+  homeBox.addEventListener('click', () => {
+    const boxId = homeBox.id; // Get the ID of the clicked box
+    const content = contentData[boxId]; // Get the corresponding content
+
+    if (content) {
+      // Update the inner HTML of .h-head and .pop-des
+      document.querySelector('.popup-content .h-head').innerHTML = content.head;
+      document.querySelector('.popup-content .pop-des').innerHTML = content.des;
+    }
+  });
+});
+
+
  // OUR CLIENTS CAROUSEL //
 
 
@@ -282,25 +323,6 @@ closeBtn.addEventListener("click", closePopup);
 
 // //   home triangle
 
-const tri1 = document.getElementById('home-tri1');
-const tri2 = document.getElementById('home-tri2');
-
-// Function to update triangle positions based on mouse movement
-document.addEventListener('mousemove', (event) => {
-    const { clientX, clientY } = event;
-
-    // Calculate translation values
-    const translateX1 = (clientX - window.innerWidth / 2) * 0.01;
-    const translateY1 = (clientY - window.innerHeight / 2) * 0.01;
-
-    const translateX2 = (clientX - window.innerWidth / 2) * 0.02;
-    const translateY2 = (clientY - window.innerHeight / 2) * 0.02;
-
-    // Apply translations
-    tri1.style.transform = `translate(${translateX1}px, ${translateY1}px)`;
-    tri2.style.transform = `translate(${translateX2}px, ${translateY2}px)`;
-});
-
 
 
 // Parallax Effect 
@@ -369,4 +391,19 @@ function setActiveLink(element) {
   }
   
 
-  
+// lottie files
+const animationData = {"v":"4.8.0","meta":{"g":"LottieFiles AE ","a":"","k":"","d":"","tc":""},"fr":30,"ip":0,"op":60,"w":500,"h":500,"nm":"27 Fast Forward","ddd":0,"assets":[],"layers":[{"ddd":0,"ind":1,"ty":3,"nm":"Main Stroke width - Color Ctrl","parent":5,"sr":1,"ks":{"o":{"a":0,"k":0,"ix":11},"r":{"a":0,"k":0,"ix":10},"p":{"a":0,"k":[0,0,0],"ix":2},"a":{"a":0,"k":[50,50,0],"ix":1},"s":{"a":0,"k":[100,100,100],"ix":6}},"ao":0,"ef":[{"ty":5,"nm":"Stroke width","np":3,"mn":"ADBE Slider Control","ix":1,"en":1,"ef":[{"ty":0,"nm":"Slider","mn":"ADBE Slider Control-0001","ix":1,"v":{"a":0,"k":16,"ix":1}}]}],"ip":0,"op":60,"st":0,"bm":0},{"ddd":0,"ind":2,"ty":4,"nm":"Arrow 3","parent":1,"sr":1,"ks":{"o":{"a":0,"k":100,"ix":11},"r":{"a":0,"k":180,"ix":10},"p":{"a":1,"k":[{"i":{"x":0.69,"y":1},"o":{"x":0.31,"y":0},"t":4,"s":[15.437,50,0],"to":[-10.667,0,0],"ti":[-9.667,0,0]},{"i":{"x":0.69,"y":1},"o":{"x":0.31,"y":0},"t":19,"s":[-48.563,50,0],"to":[9.667,0,0],"ti":[-4.667,0,0]},{"i":{"x":0.69,"y":1},"o":{"x":0.31,"y":0},"t":30,"s":[73.437,50,0],"to":[4.667,0,0],"ti":[2,0,0]},{"i":{"x":0.69,"y":1},"o":{"x":0.31,"y":0},"t":38,"s":[-20.563,50,0],"to":[-2,0,0],"ti":[-6,0,0]},{"i":{"x":0.69,"y":1},"o":{"x":0.31,"y":0},"t":46,"s":[61.437,50,0],"to":[6,0,0],"ti":[7.667,0,0]},{"t":53,"s":[15.437,50,0]}],"ix":2},"a":{"a":0,"k":[34.563,0,0],"ix":1},"s":{"a":0,"k":[100,100,100],"ix":6}},"ao":0,"shapes":[{"ty":"gr","it":[{"ind":0,"ty":"sh","ix":1,"ks":{"a":0,"k":{"i":[[0,0],[0,0],[0,0]],"o":[[0,0],[0,0],[0,0]],"v":[[171.5,171.5],[34.563,0],[171.5,-171.5]],"c":false},"ix":2},"nm":"Path 1","mn":"ADBE Vector Shape - Group","hd":false},{"ty":"st","c": { "a": 0, "k": [1, 1, 1, 1], "ix": 3 },"o":{"a":0,"k":100,"ix":4},"w":{"a":0,"k":16,"ix":5,"x":"var $bm_rt;\n$bm_rt = thisComp.layer('Main Stroke width - Color Ctrl').effect('Stroke width')('Slider');"},"lc":2,"lj":2,"bm":0,"nm":"Stroke 1","mn":"ADBE Vector Graphic - Stroke","hd":false},{"ty":"tr","p":{"a":0,"k":[0,0],"ix":2},"a":{"a":0,"k":[0,0],"ix":1},"s":{"a":0,"k":[100,100],"ix":3},"r":{"a":0,"k":0,"ix":6},"o":{"a":0,"k":100,"ix":7},"sk":{"a":0,"k":0,"ix":4},"sa":{"a":0,"k":0,"ix":5},"nm":"Transform"}],"nm":"Group 1","np":2,"cix":2,"bm":0,"ix":1,"mn":"ADBE Vector Group","hd":false}],"ip":0,"op":60,"st":0,"bm":0},{"ddd":0,"ind":3,"ty":4,"nm":"Arrow 2","parent":1,"sr":1,"ks":{"o":{"a":0,"k":100,"ix":11},"r":{"a":0,"k":180,"ix":10},"p":{"a":1,"k":[{"i":{"x":0.69,"y":1},"o":{"x":0.31,"y":0},"t":2,"s":[117.578,50,0],"to":[-10.667,0,0],"ti":[-9.667,0,0]},{"i":{"x":0.69,"y":1},"o":{"x":0.31,"y":0},"t":17,"s":[53.578,50,0],"to":[9.667,0,0],"ti":[-4.667,0,0]},{"i":{"x":0.69,"y":1},"o":{"x":0.31,"y":0},"t":28,"s":[175.578,50,0],"to":[4.667,0,0],"ti":[2,0,0]},{"i":{"x":0.69,"y":1},"o":{"x":0.31,"y":0},"t":36,"s":[81.578,50,0],"to":[-2,0,0],"ti":[-6,0,0]},{"i":{"x":0.69,"y":1},"o":{"x":0.31,"y":0},"t":44,"s":[163.578,50,0],"to":[6,0,0],"ti":[7.667,0,0]},{"t":51,"s":[117.578,50,0]}],"ix":2},"a":{"a":0,"k":[-67.578,0,0],"ix":1},"s":{"a":0,"k":[100,100,100],"ix":6}},"ao":0,"shapes":[{"ty":"gr","it":[{"ind":0,"ty":"sh","ix":1,"ks":{"a":0,"k":{"i":[[0,0],[0,0],[0,0]],"o":[[0,0],[0,0],[0,0]],"v":[[69.359,171.5],[-67.578,0],[69.359,-171.5]],"c":false},"ix":2},"nm":"Path 1","mn":"ADBE Vector Shape - Group","hd":false},{"ty":"st","c": { "a": 0, "k": [1, 1, 1, 1], "ix": 3 },"o":{"a":0,"k":100,"ix":4},"w":{"a":0,"k":16,"ix":5,"x":"var $bm_rt;\n$bm_rt = thisComp.layer('Main Stroke width - Color Ctrl').effect('Stroke width')('Slider');"},"lc":2,"lj":2,"bm":0,"nm":"Stroke 1","mn":"ADBE Vector Graphic - Stroke","hd":false},{"ty":"tr","p":{"a":0,"k":[0,0],"ix":2},"a":{"a":0,"k":[0,0],"ix":1},"s":{"a":0,"k":[100,100],"ix":3},"r":{"a":0,"k":0,"ix":6},"o":{"a":0,"k":100,"ix":7},"sk":{"a":0,"k":0,"ix":4},"sa":{"a":0,"k":0,"ix":5},"nm":"Transform"}],"nm":"Group 1","np":2,"cix":2,"bm":0,"ix":1,"mn":"ADBE Vector Group","hd":false}],"ip":0,"op":60,"st":0,"bm":0},{"ddd":0,"ind":4,"ty":4,"nm":"Arrow 1","parent":1,"sr":1,"ks":{"o":{"a":0,"k":100,"ix":11},"r":{"a":0,"k":180,"ix":10},"p":{"a":1,"k":[{"i":{"x":0.69,"y":1},"o":{"x":0.31,"y":0},"t":0,"s":[221.5,50,0],"to":[-10.667,0,0],"ti":[-9.667,0,0]},{"i":{"x":0.69,"y":1},"o":{"x":0.31,"y":0},"t":15,"s":[157.5,50,0],"to":[9.667,0,0],"ti":[-4.667,0,0]},{"i":{"x":0.69,"y":1},"o":{"x":0.31,"y":0},"t":26,"s":[279.5,50,0],"to":[4.667,0,0],"ti":[2,0,0]},{"i":{"x":0.69,"y":1},"o":{"x":0.31,"y":0},"t":34,"s":[185.5,50,0],"to":[-2,0,0],"ti":[-6,0,0]},{"i":{"x":0.69,"y":1},"o":{"x":0.31,"y":0},"t":42,"s":[267.5,50,0],"to":[6,0,0],"ti":[7.667,0,0]},{"t":49,"s":[221.5,50,0]}],"ix":2},"a":{"a":0,"k":[-171.5,0,0],"ix":1},"s":{"a":0,"k":[100,100,100],"ix":6}},"ao":0,"shapes":[{"ty":"gr","it":[{"ind":0,"ty":"sh","ix":1,"ks":{"a":0,"k":{"i":[[0,0],[0,0],[0,0]],"o":[[0,0],[0,0],[0,0]],"v":[[-34.564,171.5],[-171.5,0],[-34.564,-171.5]],"c":false},"ix":2},"nm":"Path 1","mn":"ADBE Vector Shape - Group","hd":false},{"ty":"st","c": { "a": 0, "k": [0.5, 0.5, 0.5, 1], "ix": 3 },"o":{"a":0,"k":100,"ix":4},"w":{"a":0,"k":16,"ix":5,"x":"var $bm_rt;\n$bm_rt = thisComp.layer('Main Stroke width - Color Ctrl').effect('Stroke width')('Slider');"},"lc":2,"lj":2,"bm":0,"nm":"Stroke 1","mn":"ADBE Vector Graphic - Stroke","hd":false},{"ty":"tr","p":{"a":0,"k":[0,0],"ix":2},"a":{"a":0,"k":[0,0],"ix":1},"s":{"a":0,"k":[100,100],"ix":3},"r":{"a":0,"k":0,"ix":6},"o":{"a":0,"k":100,"ix":7},"sk":{"a":0,"k":0,"ix":4},"sa":{"a":0,"k":0,"ix":5},"nm":"Transform"}],"nm":"Group 1","np":2,"cix":2,"bm":0,"ix":1,"mn":"ADBE Vector Group","hd":false}],"ip":0,"op":60,"st":0,"bm":0},{"ddd":0,"ind":5,"ty":3,"nm":"Zero | troke width - Color Ctrl","sr":1,"ks":{"o":{"a":0,"k":0,"ix":11},"r":{"a":0,"k":0,"ix":10},"p":{"a":0,"k":[250,250,0],"ix":2},"a":{"a":0,"k":[0,0,0],"ix":1},"s":{"a":0,"k":[100,100,100],"ix":6}},"ao":0,"ip":0,"op":60,"st":0,"bm":0}],"markers":[]};
+
+const arrowElements = document.querySelectorAll('.home-arrow');
+
+// Initialize the Lottie animation for each element
+arrowElements.forEach(arrowElement => {
+  lottie.loadAnimation({
+    container: arrowElement, // Target each container
+    renderer: 'svg',         // Render as SVG
+    loop: true,              // Loop the animation
+    fr: 15,                  // Frame rate
+    autoplay: true,          // Start automatically
+    animationData: animationData // Lottie JSON data
+  });
+});
